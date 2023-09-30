87e0e4c5fe8bb03075e4639de0bee8536c14795f
Shreeraam G <133973475+shreeraamg@users.noreply.github.com>
2023-09-30 09:12:32 +0530

    Made by credits

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,17 @@ import Logo from "./Logo";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
+    <footer className="footer p-10 bg-base-200 text-base-content relative">
+      <p className="inline absolute bottom-2 right-2">
+        Made by{" "}
+        <Link
+          className="text-cyan-700"
+          target="_blank"
+          href="https://www.linkedin.com/in/shreeraam"
+        >
+          Shreeraam G
+        </Link>
+      </p>
       <aside>
         <Logo />
         <ul className="space-y-2">
