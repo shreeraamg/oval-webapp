343707ba90c90c167a7341121b46d85871f3cee9
Shreeraam G <133973475+shreeraamg@users.noreply.github.com>
2023-10-09 21:38:11 +0530

    Social media handle assets

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -3,6 +3,11 @@ type website = {
   company: string;
 };
 
+type poster = {
+  imageSrc: string;
+  tag: string;
+}
+
 export const websites: website[] = [
   { company: "i1 Sports", imageSrc: "/websites/project3.jpeg" },
   { company: "SSV Traders", imageSrc: "/websites/project7.jpeg" },
@@ -10,3 +15,26 @@ export const websites: website[] = [
   { company: "Techkartpro.com", imageSrc: "/websites/project10.jpeg" },
   { company: "The Journey Guru", imageSrc: "/websites/project11.jpeg" },
 ];
+
+export const socialMediaPosters: poster[] = [
+  {tag: "Branding", imageSrc: "/socialmedia/branding1.jpg"},
+  {tag: "Informative Post", imageSrc: "/socialmedia/infopost1.jpg"},
+  {tag: "Audience Engagement", imageSrc: "/socialmedia/audienceengagement1.jpg"},
+  {tag: "Animation Video Editing", imageSrc: "/socialmedia/animationvideoediting1.jpg"},
+  {tag: "Branding", imageSrc: "/socialmedia/branding2.jpg"},
+  {tag: "Informative Post", imageSrc: "/socialmedia/infopost2.jpg"},
+  {tag: "Engagement Memes", imageSrc: "/socialmedia/engagementmeme1.jpg"},
+  {tag: "Service Showcasing", imageSrc: "/socialmedia/serviceshowcase1.jpg"},
+  {tag: "Branding", imageSrc: "/socialmedia/branding3.jpg"},
+  {tag: "Informative Post", imageSrc: "/socialmedia/infopost3.jpg"},
+  {tag: "Audience engagement", imageSrc: "/socialmedia/audienceengagement2.jpg"},
+  {tag: "Promotion", imageSrc: "/socialmedia/promotion1.jpg"},
+  {tag: "Branding", imageSrc: "/socialmedia/branding4.jpg"},
+  {tag: "Informative Post", imageSrc: "/socialmedia/infopost4.jpg"},
+  {tag: "Service Showcasing", imageSrc: "/socialmedia/serviceshowcase2.jpg"},
+  {tag: "Personal branding Videos", imageSrc: "/socialmedia/personalbrandingvideo1.jpg"},
+  {tag: "Branding", imageSrc: "/socialmedia/branding5"},
+  {tag: "Informative Post", imageSrc: "/socialmedia/infopost5"},
+  {tag: "Meme Reels", imageSrc: "/socialmedia/memereel1.jpg"},
+  {tag: "Corporate Branding", imageSrc: "/socialmedia/corporatebranding1.jpg"}, 
+]
